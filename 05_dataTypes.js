ee3afe8699532bb8d38f5325636315ef0e5e541d
samirulcodes <samirulislam.official.com>
2024-01-06 11:45:24 +0530

    dataTypes

---
+++ 05_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Aman")
+
+let name="aman" //string
+let age=20 //integer
+let isLoggedIn=false //booloean
+
+// number=> 2 to power 53(range)
+// bigint ( for storing biggest number)
+// string=""
+// boolean=> true/false
+// null=> standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof "aman")
+console.log(typeof "age")
+console.log(typeof "null")
+console.log(typeof "undefined")
