c63953a66d6946e34fa045e67797b4c6b3b8e95f
samirulcodes <samirulislam.official.com>
2024-01-09 20:14:47 +0530

    object_destructure and basic JSON_API intro

---
+++ 18_object_destructureAndJSON_API.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename: "java script",
+    price: "999",
+    courseInstructor: "hitesh"
+
+}
+// course.courseInstructor
+
+const { courseInstructor: instructor } = course
+// console.log(courseInstructor); //o/p--> hitesh
+console.log(instructor); //o/p-->hitesh
+
+// API intro
+
+// {
+//     "name": "aman",
+//     "coursename": "java script",
+//     price: "free",
+    
+// }
+
+[
+    {},
+    {},
+    {},
+]
