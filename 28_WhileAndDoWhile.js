2420f915249ca2f30e3010ea361ab1450f3549a2
samirulcodes <samirulislam.official.com>
2024-03-10 11:42:04 +0530

    while , do-while loop in js

---
+++ 28_WhileAndDoWhile.js
@@ -0,0 +1,25 @@
+// While Loop
+
+let i=1
+while(i<=10){
+    // console.log(`value of i is ${i}`);
+    i++
+    // i=i+2 // this will increment by 2
+}
+
+let myArr = ["Aman", "Samirul", "Hitesh"]
+
+let arr=0
+while(arr<myArr.length){
+    // console.log(`value is ${myArr[arr]}`);
+    arr++
+}
+
+// Do-While Loop
+
+let score=1
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10);
