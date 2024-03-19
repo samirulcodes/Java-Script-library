972114eb49f67643879a45967e220529dde25bc2
samirulcodes <samirulislam.official.com>
2024-03-19 22:11:09 +0530

    clock js

---
+++ Project 3/clock.js
@@ -0,0 +1,16 @@
+// const clock=document.querySelector('#clock')
+const clock=document.getElementById('clock')
+
+setInterval(function(){
+    let date=new Date
+    clock.innerHTML=date.toLocaleTimeString()
+},1000)
+
+
+// setInterval()--> ye methods time ko continously run krta rhega jab time web open rhega
+
+// toLocaleTimeString()--> this will provide only cureent time
+
+// toLocaleDateString()--> this will provide only cureent date
+
+// toLocaleString()--> this will provide both cureent date and current time
