297e9c2a53801494abd568c474de0277fbca031f
samirulcodes <samirulislam.official.com>
2024-03-19 22:13:20 +0530

    project 3 js

--- Project 3/clock.js
+++ Project 3/clock.js
@@ -4,7 +4,7 @@ const clock=document.getElementById('clock')
 setInterval(function(){
     let date=new Date
     clock.innerHTML=date.toLocaleTimeString()
-},1000
+},1000)
 
 
 // setInterval()--> ye methods time ko continously run krta rhega jab time web open rhega
